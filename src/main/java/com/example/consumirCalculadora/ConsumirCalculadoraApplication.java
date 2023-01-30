ed95fe72b90fe7cce405912054d93700d5d4a9cb
rmanjarres <rafaelmanjarres1995@gmail.com>
2023-01-30 18:09:32 -0500

    Ajustando código

--- src/main/java/com/example/consumirCalculadora/ConsumirCalculadoraApplication.java
+++ src/main/java/com/example/consumirCalculadora/ConsumirCalculadoraApplication.java
@@ -4,15 +4,10 @@ import com.example.consumirCalculadora.service.ConsumirCalculadoraService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
-import java.util.HashMap;
-import java.util.Map;
 
 @SpringBootApplication
 @RestController
